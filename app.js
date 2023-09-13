9a9003a8027658b126aa0ab88c9e3da03b7208f9
unknown <oolalere2015@gmail.com>
2023-09-13 21:45:39 +0100

    feat: implementing GET route for /api/stuff

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const express = require('express');
 
 const app = express();
 
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'aholiabbezeleel1',
+            title: 'My first thing',
+            description: 'All of the info about my first thing',
+            imageUrl: '',
+            price: 4900,
+            userId: 'phineas'
+        },
+        {
+            _id: 'aholiabbezeleel12',
+            title: 'My second thing',
+            description: 'All of the info about my second thing',
+            imageUrl: '',
+            price: 2900,
+            userId: 'nathan'
+        }
+    ];
+
+    res.status(200).json(stuff);
+});
+
+/* MIDDLEWARE DEMO STARTS HERE 
+
 app.use((req, res, next) => {
     console.log('Request Received!');
     next();
@@ -22,4 +47,8 @@ app.use((req, res, next) => {
     // next();
 });
 
+MIDDLEWARE DEMO STARTS HERE */
+
 module.exports = app;
+
+
