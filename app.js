b0db2394bda23c555ad903d7261465673510d59f
unknown <oolalere2015@gmail.com>
2023-09-19 18:46:58 +0100

    refactor: set up routing to optimize backend structure

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/thing');
+const stuffRoutes = require('./routes/stuff');
 // const cors = require('cors');
 
 const app = express();
@@ -41,87 +41,8 @@ mongoose.connect('mongodb+srv://oluconquer:kFnpRl3DhSFIUbK5@oluconquer.jyksbsn.m
     console.log('Unable to connect to MongoDB Community Atlas!');
     console.error(error);
 });
-  
-// Saving Things for sale  to the Database
-app.post('/api/stuff', (req, res, next) => {
-    // console.log(req.body); // before implementing Thing model
-    // res.status(201).json({message: 'Thing created successfully!'}); // before implementing Thing model
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
 
-    thing.save().then(
-         () => {
-        res.status(201).json({message: 'Post saved successfully!'});
-    })
-    .catch( 
-        (error) => {
-        res.status(400).json({
-            error: error
-        });
-    }
-    );
-});
-
-// Retrieving a Specific Thing
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({_id: req.params.id})
-    .then((thing) => {
-        res.status(200).json(thing);
-    })
-    .catch((error) => {
-        res.status(404).json({error: error});
-    });
-});
-
-// Updating an Existing Thing
-app.put('/api/stuff/:id', (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-
-    Thing.updateOne({_id: req.params.id}, thing)
-    .then( () => {
-        res.status(201).json({message: 'Thing updated succesfully!'});
-    })
-    .catch( (error) => {
-        res.status(400).json({error: error});
-    });
-});
-
-// Deleting an Existing Thing
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id})
-    .then( () => {
-        res.status(200).json({message: 'Deleted!'});
-    })
-    .catch( (error) => {
-        res.status(400).json({error: error});
-    });
-});
-
-// Retrieving the List of all Things for Sale
-
-// app.use('/api/stuff', (req, res, next) => { /* THIS LINE CAUSED CORS ERRORS THAT TOOK A WHOLE DAY TO DEBUG */
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then((things) => {
-        res.status(200).json(things);
-    })
-    .catch((error) => {
-        res.status(400).json({error: error});
-    });
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
 
