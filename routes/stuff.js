4d3fdd2a875185ca58ea71969b9ef4d26578eceb
unknown <oolalere2015@gmail.com>
2023-09-22 18:11:53 +0100

    feat: apply authentication middleware to stuffRoutes

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const Thing = require('../models/thing');
-
 const router = express.Router();
+
+const auth = require('../middleware/auth');
 
 const stuffCtrl = require('../controllers/stuff');
 
+// Retrieving the List of all Things for Sale
+router.get('/', stuffCtrl.getAllStuff);
+
 // Saving Things for sale  to the Database
-router.post('/', stuffCtrl.createThing), 
+router.post('/', auth, stuffCtrl.createThing), 
 
 // Retrieving a Specific Thing
 router.get('/:id', stuffCtrl.getOneThing);
 
 // Updating an Existing Thing
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
 
 // Deleting an Existing Thing
-router.delete('/:id', stuffCtrl.deleteThing);
-
-// Retrieving the List of all Things for Sale
-router.get('/', stuffCtrl.getAllStuff);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
